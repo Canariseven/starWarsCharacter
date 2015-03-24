ab8139f6c8e202e17f2f83f6ad065e79f8408eed
Carmelo Ruymán Quintana Santana <canariseven@gmail.com>
2015-03-24 21:33:59 +0000

    First commit, orange windows

---
+++ StarWars/StarWars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarWars
+//
+//  Created by Carmelo Ruymán Quintana Santana on 24/3/15.
+//  Copyright (c) 2015 Carmelo Ruymán Quintana Santana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
